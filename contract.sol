a28c08c2e7c43e6be82287645c7caec8f3a7b1e4
Yasin Aktimur <yasinaktimur@gmail.com>
2025-08-22 16:25:00 +0300

    sc

--- contract.sol
+++ contract.sol
@@ -4,21 +4,18 @@ pragma solidity ^0.8.24;
 contract flooordotfun {
     address public owner;
     modifier onlyOwner(){ require(msg.sender==owner,"owner"); _; }
-    //uint256 public plusone;
-    uint256 private  constant BLOCK   = 6 minutes; 
-    uint256 private constant SIGN_DURATION = 4 minutes; 
+    uint256 private  constant rBLOCKS   = 2 minutes; 
+    uint256 private constant sDURTION = 1 minutes; 
 
-    //function incrase() public {
-    //    plusone = plusone + 1;
-   // }
+
+    
 
     function signorclaim() public view returns (uint256 secondsLeft, bool isSignPhase, string memory phaseName, uint256 btt) {
-        uint256 modTime = block.timestamp % 360; // 2 dk döngü
-        if (modTime < 60) {
-            return (60 - modTime, false, "sign", block.timestamp);
+        uint256 modTime = block.timestamp % rBLOCKS; 
+        if (modTime < sDURTION) {
+            return (sDURTION - modTime, false, "sign", block.timestamp);
         } else {
-            // you can claim right now claim mode : true
-            return (360 - modTime, true, "claim", block.timestamp);
+            return (rBLOCKS - modTime, true, "claim", block.timestamp);
         }
     }
  
